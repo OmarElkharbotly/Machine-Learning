f2b3d0fbde2bf69ab7f2edd9eb4e9e6c25fdcbf2
OmarElkharbotly <47695221+OmarElkharbotly@users.noreply.github.com>
2019-02-16 17:51:31 +0200

    Add files via upload

---
+++ LogisticalHyp2.m
@@ -0,0 +1,68 @@
+%Hyp1 Using 4 Variables linear and square              no regularization. 
+
+clear all   
+ds = datastore('heart_DD.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',25000);
+T = read(ds);
+size(T);
+Alpha=.001;
+m=length(T{1:175,1});
+mTest=length(T{176:end,1});
+% U0=T{:,2};
+U=T{1:175,[5 6 7 8]};
+UTest=T{176:end,[5 6 7 8]};
+% U1=T{:,13:14};
+% U2=T{:,20:21};
+% X=[ones(m,1) U U1 U.^2 U.^3];
+X=[ones(m,1) U U.^2];
+XTest=[ones(mTest,1) UTest UTest.^2];
+
+
+n=length(X(1,:));
+for w=2:n   
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+end
+
+nTest=length(XTest(1,:));
+for w=2:nTest    
+    if max(abs(XTest(:,w)))~=0
+    XTest(:,w)=(XTest(:,w)-mean((XTest(:,w))))./std(XTest(:,w));
+    end
+end
+
+Y=T{1:175,14}/mean(T{1:175,14});
+YTest=T{176:end,14}/mean(T{1:175,14});
+Theta=zeros(n,1);
+k=1;
+Y=Y';
+XTest=XTest';
+X=X';
+YTest=YTest';
+E(k)=-(1/(m))*sum((Y.*log(1./(1+exp((-Theta')*X))))+((1-Y).*log(1./(1+exp((-Theta')*X))))); %Calculation Error (Cost function)
+
+R=1;
+while R==1
+Alpha=Alpha*1;
+Theta=Theta-(Alpha/m)*X*(((log(1./(1+exp((-Theta')*X))))-Y)');
+k=k+1
+E(k)=-(1/(m))*sum((Y.*log(1./(1+exp((-Theta')*X))))+((1-Y).*log(1./(1+exp((-Theta')*X)))));
+if E(k-1)-E(k)<0
+    break
+end 
+q=(E(k-1)-E(k))./E(k-1);
+if q <.000001;
+    R=0;
+end
+end
+figure(1)
+plot(E)
+
+ Etest=-(1/(mTest))*sum((YTest.*log(1./(1+exp((-Theta')*XTest))))+((1-YTest).*log(1./(1+exp((-Theta')*XTest)))));
+TargetNew=(1./(1+exp((-Theta')*XTest)));
+
+% MSE=((TargetNew-YTest).^2)/(mTest);
+% MSEsum=sum(MSE);
+% figure(2)
+% plot(MSE)
---
+++ ML_Assignment1.m
@@ -0,0 +1,70 @@
+%First Hypothesis By Using 1 normalized Variable and Squaring it
+
+
+clc
+close all
+clear all
+ds = tabularTextDatastore('house_data_complete.csv','TreatAsMissing','NA',.....
+    'MissingValue',0,'ReadSize',25000);
+T = read(ds);
+size(T);
+Alpha=.01;
+
+
+U0=T{1:18000,2} ;  % Data 2
+%U=T{1:18000,4:19};    % Data from 4 to 19
+
+U1=T{1:18000,20:21};    %Data 20 and 21
+%U2=U.^2;
+%U3= U.^3;
+
+UTrain = T{1:18000,6};
+UTest = T{18001:end,6};
+m=length(UTrain);
+mTest=length(UTest);
+%X=[ones(m,1) U U1 U.^2 U.^3];     
+X=[ones(m,1) UTrain UTrain.^2];
+XTest=[ones(mTest,1) UTest UTest.^2];
+
+n=length(X(1,:));
+for w=2:n
+    if max(abs(X(:,w)))~=0
+    X(:,w)=(X(:,w)-mean((X(:,w))))./std(X(:,w));
+    end
+end
+f = length(XTest(1,:));
+for w=2:f
+    if max(abs(XTest(:,w)))~=0
+    XTest(:,w)=(XTest(:,w)-mean((XTest(:,w))))./std(XTest(:,w));
+    end
+end
+
+YTrain=T{1:18000,3}/mean(T{1:18000,3});
+YTest=T{18001:end,3}/mean(T{1:18000,3});
+Theta=zeros(n,1);
+k=1;
+
+E(k)=(1/(2*m))*sum((X*Theta-YTrain).^2);   %Error Calculations (Cost Function).
+
+R=1;
+while R==1
+Alpha=Alpha*1;
+Theta=Theta-(Alpha/m)*X'*(X*Theta-YTrain);  %Updating All Thetas using matrix format.
+k=k+1
+E(k)=(1/(2*m))*sum((X*Theta-YTrain).^2);    %New Cost Function.
+if E(k-1)-E(k)<0
+    break
+end 
+q=(E(k-1)-E(k))./E(k-1);               %Diff between old and new errors in percentage.
+if q <.000001;
+    R=0;
+end
+end
+
+YNew=((XTest*Theta))
+
+figure()
+plot(E)
+
+
+MSE=sum((YTest-YNew).^2)/(2*mTest) ;
